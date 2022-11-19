60ec3d8e6954d539f2152d61c37cf9416f4bcf64
ll835 <loktinlo@gmail.com>
2022-11-19 23:30:00 +0000

    tidy up displayResults()

--- script.js
+++ script.js
@@ -29,9 +29,11 @@
 /* 
 <================== Code to run sample games ==================>
  */
-
+const form = document.querySelector("form");
+const noOfGames = document.querySelector("#no-of-games");
+const chosenStrategy = document.querySelector("#strategy") 
 const doors = [1, 2, 3];
-const results = [];
+let results = [];
 
 function runSimulation(noOfRounds, strategy){
     for (let i = 0; i < noOfRounds; i++) {
@@ -40,7 +42,7 @@ function runSimulation(noOfRounds, strategy){
         const outcome = useStrategy(strategy, playerDoor, prizeDoor);
         results.push(outcome);
     }
-    summariseResults(results)
+    displayResults();
 }
 
 function pickRandomDoor(arr){
@@ -65,48 +67,84 @@ function useStrategy(stickOrSwitch, guess, actual){
 }
 
 function summariseResults(arr){
+    const strategy = chosenStrategy.value
     const noOfCars = arr.filter(element => element.result === "win").length;
     const noOfGoats = arr.filter(element => element.result=== "lose").length;
+    const totalNo = noOfCars + noOfGoats
     const percentage = ((noOfCars / arr.length) * 100);
     const winrate = + percentage.toFixed(2) + "%";
-    console.log({noOfCars, noOfGoats, winrate})
+    return {strategy, noOfCars, noOfGoats, winrate, totalNo}
 }
 
 function displayResults(){
+    const output = document.createElement("output");
+
+    output.classList.add("output");
+    output.classList.add("v-spacing-md");
+    const summary = summariseResults(results);
+    const summaryCtn = document.createElement("div");
+    const strategy = document.createElement("p");
+    let text = ""
+    summary.strategy === "stick" ? text = "Stay with the first door" : text = "Switch to the other door"
+    strategy.textContent = `Strategy: ${text}`;
+    const winrate = document.createElement("p");
+    winrate.textContent = `Winrate: ${summary.winrate}`;
+    const totalNo = document.createElement("p");
+    totalNo.textContent = `Games played: ${summary.totalNo}`;
+    const goats = document.createElement("p");
+    goats.textContent = `Goats found: ${summary.noOfGoats}`;
+    const cars = document.createElement("p");
+    cars.textContent = `Cars found: ${summary.noOfCars}`;
+    summaryCtn.appendChild(strategy);
+    summaryCtn.appendChild(winrate);
+    summaryCtn.appendChild(totalNo);
+    summaryCtn.appendChild(goats);
+    summaryCtn.appendChild(cars);
+    output.append(summaryCtn)
+
+
     const table = document.createElement("table");
     const headingsRow = document.createElement("tr") 
-    const columnNames = ["Game No", "Guessed Door No", "Winning Door No.", "Result"]
+    const columnNames = ["Game no.", "Door picked", "Door with car", "Result"]
     columnNames.forEach(name => {
         const heading = document.createElement("th");
         heading.innerText = name;
         headingsRow.appendChild(heading);
     })
     table.appendChild(headingsRow)
-
     for (let i = 0; i < results.length; i++){
         const newRow = document.createElement("tr")
-
         const gameNoCell = document.createElement("td");
         gameNoCell.innerText = i + 1;
         newRow.appendChild(gameNoCell)
-
         const guessedCell = document.createElement("td");
         guessedCell.innerText = results[i].guess;
         newRow.appendChild(guessedCell)
-
-
         const actualCell = document.createElement("td");
         actualCell.innerText = results[i].actual
         newRow.appendChild(actualCell)
-
         const resultsCell = document.createElement("td");
-        resultsCell.innerText = results[i].result
+        resultsCell.innerText = `${results[i].result}`
         newRow.appendChild(resultsCell)
-
         table.appendChild(newRow)
     }
-    document.body.appendChild(table)
+    output.appendChild(table);
+
+    const main = document.querySelector("main");
+    main.appendChild(output)
 }
 
-runSimulation(30, "switch")
-displayResults()
+function resetPage(){
+    if (document.body.contains(document.querySelector("output"))) {
+        document.querySelector("output").remove()
+    }
+    results = [];
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    resetPage();
+    setTimeout(() => {
+        runSimulation(noOfGames.value,  chosenStrategy.value)
+    }, 200)
+})
