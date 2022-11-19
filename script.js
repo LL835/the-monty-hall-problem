f31978c5839ad16fa59191253a3dfb64898ab919
ll835 <loktinlo@gmail.com>
2022-11-19 18:54:00 +0000

    draft functions to summarise and display results

--- script.js
+++ script.js
@@ -30,136 +30,6 @@
 <================== Code to run sample games ==================>
  */
 
-// const doors = [1, 2, 3];
-// const results = 
-// {
-//     strategy: "",
-//     rounds: "",
-//     cars: 0,
-//     goats: 0,
-//     winRate: "",
-// }
-
-// function runSampleGame(strategy, rounds){
-//     for (let i = 0; i < rounds; i++){
-//         const winningDoor = getRandomItemFromArr(doors);
-//         const contestantChoice = getRandomItemFromArr(doors);
-//         getResults(strategy, contestantChoice, winningDoor);
-//         calculateWinRate(rounds, results.cars);
-//     }
-//     results.strategy = strategy;
-//     results.rounds = rounds;
-//     // console.log(results);
-//     presentResults();
-//     resetResults();
-// }
-
-// function getRandomItemFromArr(array){
-//     const randomIndex = Math.floor(Math.random() * array.length);
-//     return array[randomIndex];
-// }
-
-// function getResults(strategy, guess, actual){
-//     if (strategy === "stick"){
-//         stickWithChoice(guess, actual)
-//     }
-//     else if (strategy === "switch"){
-//         switchChoice(guess, actual)
-//     }
-// }
-
-// function stickWithChoice(guess, actual){
-//     if (guess === 1 && actual === 1){
-//         results.cars += 1;
-//     }
-//     else if (guess === 1 && actual === 2){
-//         results.goats += 1;
-//     }
-//     else if (guess === 1 && actual === 3){
-//         results.goats += 1;
-//     }
-
-//     else if (guess === 2 && actual === 1){
-//         results.goats += 1;
-//     }
-//     else if (guess === 2 && actual === 2){
-//         results.cars += 1;
-//     }
-//     else if (guess === 2 && actual === 3){
-//         results.goats += 1;
-//     }
-
-//     else if (guess === 3 && actual === 1){
-//         results.goats += 1;
-//     }
-//     else if (guess === 3 && actual === 2){
-//         results.goats += 1;
-//     }
-//     else if (guess === 3 && actual === 3){
-//         results.cars += 1;
-//     }
-// }
-
-// function switchChoice(guess, actual){
-//     if (guess === 1 && actual === 1){
-//         results.goats += 1;
-//     }
-//     else if (guess === 1 && actual === 2){
-//         results.cars += 1;
-//     }
-//     else if (guess === 1 && actual === 3){
-//         results.cars += 1;
-//     }
-
-//     else if (guess === 2 && actual === 1){
-//         results.cars += 1;
-//     }
-//     else if (guess === 2 && actual === 2){
-//         results.goats += 1;
-//     }
-//     else if (guess === 2 && actual === 3){
-//         results.cars += 1;
-//     }
-
-//     else if (guess === 3 && actual === 1){
-//         results.cars += 1;
-//     }
-//     else if (guess === 3 && actual === 2){
-//         results.cars += 1;
-//     }
-//     else if (guess === 3 && actual === 3){
-//         results.goats += 1;
-//     }
-// }
-
-// function calculateWinRate(total, wins){
-//     let percentage = (parseFloat(wins) / parseFloat(total) * 100);
-//     percentage = +percentage.toFixed(2);
-//     results.winRate = `${percentage}%`
-// }
-
-// function resetResults(){
-//     results.strategy = "";
-//     results.rounds =  "";
-//     results.cars =  0;
-//     results.goats =  0;
-//     results.winRate =  "";
-// }
-
-// function presentResults(){
-//     console.group(`Strategy employed: ${results.strategy}`)
-//         console.log(`In ${results.rounds} games, the contestant won ${results.winRate} of the time`);
-//         console.log(`The contestant picked the door with the car ${results.cars} times`);
-//         console.log(`The contestant picked a door with a goat ${results.goats} times`)
-//     console.groupEnd();
-// }
-// runSampleGame("stick", 100);
-// runSampleGame("switch", 100);
-// runSampleGame("stick", 300);
-// runSampleGame("switch", 300);
-// runSampleGame("stick", 1234);
-// runSampleGame("switch", 1234);
-
 const doors = [1, 2, 3];
 const results = [];
 
@@ -170,7 +40,7 @@ function runSimulation(noOfRounds, strategy){
         const outcome = useStrategy(strategy, playerDoor, prizeDoor);
         results.push(outcome);
     }
-    console.log(processResults(results));
+    summariseResults(results)
 }
 
 function pickRandomDoor(arr){
@@ -179,42 +49,64 @@ function pickRandomDoor(arr){
 }
 
 function useStrategy(stickOrSwitch, guess, actual){
+    const outcome = {guess, actual}
     switch(stickOrSwitch){
         case "stick":
-            return stickWithSameDoor(guess, actual);
+            guess === actual ? outcome.result = "win" : outcome.result = "lose"
             break; 
         case "switch":
-            return switchToDiffDoor(guess, actual);
+            guess === actual ? outcome.result = "lose" : outcome.result = "win"
+
             break;
         default:
             console.error("Oops, something's gone wrong in useStrategy()!")
     }
+    return outcome
 }
 
-function stickWithSameDoor(guess, actual){
-    if (guess === actual){
-        return "win";
-        }
-    else {
-        return "lose";
+function summariseResults(arr){
+    const noOfCars = arr.filter(element => element.result === "win").length;
+    const noOfGoats = arr.filter(element => element.result=== "lose").length;
+    const percentage = ((noOfCars / arr.length) * 100);
+    const winrate = + percentage.toFixed(2) + "%";
+    console.log({noOfCars, noOfGoats, winrate})
+}
+
+function displayResults(){
+    const table = document.createElement("table");
+    const headingsRow = document.createElement("tr") 
+    const columnNames = ["Game No", "Guessed Door No", "Winning Door No.", "Result"]
+    columnNames.forEach(name => {
+        const heading = document.createElement("th");
+        heading.innerText = name;
+        headingsRow.appendChild(heading);
+    })
+    table.appendChild(headingsRow)
+
+    for (let i = 0; i < results.length; i++){
+        const newRow = document.createElement("tr")
+
+        const gameNoCell = document.createElement("td");
+        gameNoCell.innerText = i + 1;
+        newRow.appendChild(gameNoCell)
+
+        const guessedCell = document.createElement("td");
+        guessedCell.innerText = results[i].guess;
+        newRow.appendChild(guessedCell)
+
+
+        const actualCell = document.createElement("td");
+        actualCell.innerText = results[i].actual
+        newRow.appendChild(actualCell)
+
+        const resultsCell = document.createElement("td");
+        resultsCell.innerText = results[i].result
+        newRow.appendChild(resultsCell)
+
+        table.appendChild(newRow)
     }
+    document.body.appendChild(table)
 }
 
-function switchToDiffDoor(guess, actual){
-    if (guess === actual){
-        return "lose";
-    }
-    else {
-        return "win";
-    }
-}
-
-function processResults(arr){
-    const noOfCars = arr.filter(element => element === "win").length;
-    const noOfGoats = arr.filter(element => element === "lose").length;
-    let percentage = ((noOfCars / arr.length) * 100);
-    percentage = + percentage.toFixed(2);
-    return {noOfCars, noOfGoats, percentage}
-}
-
-runSimulation(100, "stick")
+runSimulation(30, "switch")
+displayResults()
